08cd503abd13ea85cadd7f51bc58c521971634ba
Pawel Kawula <pawel.kawula@merit-automotive.com>
2024-10-21 21:40:25 +0200

    Refactor main.cpp to use MqttCfg object and unique_ptr for SysMonitor

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,20 @@
 #include <csignal>
 #include <thread>
 #include <chrono>
+#include <memory>
 
 #include "SysMonitor.h"
 
 #include "logger.h"
 
-
-// std::string g_mqtt_server = "localhost";
-// std::string g_mqtt_port = "1883";
-// std::string g_mqtt_root_topic = "symon";
+const MqttCfg g_cfg("sys_mon", "localhost", 1883, 60);
+static std::unique_ptr<SysMonitor> g_system_monitor;
 
 static void signalHandler(int signum);
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-
-    Logger::Initialize(LOG_DEBUG, "sys_mon");
+    Logger::Initialize(LOG_DEBUG, g_cfg._clientName.c_str());
     Logger::LogNotice("System monitor is starting...");
 
     // Register signal handler
@@ -26,29 +23,22 @@ int main(int argc, char *argv[])
     std::signal(SIGTERM, signalHandler);
     std::signal(SIGSEGV, signalHandler);
 
-    MqttCfg cfg("sys_mon", "localhost", 1883, 60);
-    SysMonitor system_monitor{cfg};
-
-    if (system_monitor.Initialize() != 0)
+    g_system_monitor = std::make_unique<SysMonitor>(g_cfg);
+    if (g_system_monitor->Initialize() != 0)
     {
         Logger::LogError("Failed to initialize system monitor");
         return 1;
     }
 
-    // Keep the main thread alive to allow the network loop to run
-
-    // client.disconnect();
     Logger::Deinit();
 
     return 0;
 }
 
-// in case of SEGFAULT
-
+// TODO: Implement signal handlers for SIGINT, SIGTERM, and SIGSEGV
 static void signalHandler(int signum)
 {
     std::cerr << "Signal " << signum << " received" << std::endl;
     Logger::Deinit();
     exit(signum);
 }
-
