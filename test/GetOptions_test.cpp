853500ad3d2f94ccdefa1ba741c6b64187c566c2
Pawel Kawula <pawel.kawula@merit-automotive.com>
2024-10-29 10:47:59 +0100

    Refactor GetOptions_test.cpp - Add test fixtures and improve test cases

--- test/GetOptions_test.cpp
+++ test/GetOptions_test.cpp
@@ -14,10 +14,36 @@
 
 const MqttCfg mqtt_cfg_vanilla("client_name", "host_address", 1111, 22, "user_name", "user_password", "root_topic");
 
-TEST(GetOptions, GetOptions)
-{
-    MqttCfg mqtt_cfg = mqtt_cfg_vanilla;
+class GetOptionsTest : public ::testing::Test {
+protected:
+    MqttCfg mqtt_cfg;
 
+    void SetUp() override {
+        mqtt_cfg = mqtt_cfg_vanilla;
+    }
+
+    void TearDown() override {
+        // Clean up resources if needed
+    }
+
+    int checkIfMqttCfgIsNotChanged(const MqttCfg &mqtt_cfg, const MqttCfg &mqtt_cfg_vanilla)
+    {
+        // Check if the mqtt_cfg is not changed
+        if (mqtt_cfg._clientName != mqtt_cfg_vanilla._clientName ||
+            mqtt_cfg._host != mqtt_cfg_vanilla._host ||
+            mqtt_cfg._port != mqtt_cfg_vanilla._port ||
+            mqtt_cfg._keepAlive != mqtt_cfg_vanilla._keepAlive ||
+            mqtt_cfg._username != mqtt_cfg_vanilla._username ||
+            mqtt_cfg._password != mqtt_cfg_vanilla._password)
+        {
+            return -1;
+        }
+        return 0;
+    }
+};
+
+TEST_F(GetOptionsTest, GetOptions)
+{
     int argc = 7;
     char *argv[] = {
         (char *)"test",
@@ -39,9 +65,8 @@ TEST(GetOptions, GetOptions)
     EXPECT_EQ(mqtt_cfg._password, "user_password");
 }
 
-TEST(GetOptions, GetOptionsInvalid)
+TEST_F(GetOptionsTest, GetOptionsInvalid)
 {
-    MqttCfg mqtt_cfg("client_name", "host_address", 1111, 22, "user_name", "user_password", "root_topic");
     int argc = 8;
     char *argv[] = {
         (char *)"test",
@@ -55,18 +80,11 @@ TEST(GetOptions, GetOptionsInvalid)
 
     EXPECT_EQ(getOptions(mqtt_cfg, argc, argv), -1);
 
-    // Check if the mqtt_cfg is not changed
-    EXPECT_EQ(mqtt_cfg._clientName, mqtt_cfg_vanilla._clientName);
-    EXPECT_EQ(mqtt_cfg._host, mqtt_cfg_vanilla._host);
-    EXPECT_EQ(mqtt_cfg._port, mqtt_cfg_vanilla._port);
-    EXPECT_EQ(mqtt_cfg._keepAlive, mqtt_cfg_vanilla._keepAlive);
-    EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
-    EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
+    EXPECT_EQ(checkIfMqttCfgIsNotChanged(mqtt_cfg, mqtt_cfg_vanilla), 0);
 }
 
-TEST(GetOptions, GetOptionsHelp)
+TEST_F(GetOptionsTest, GetOptionsHelp)
 {
-    MqttCfg mqtt_cfg("client_name", "host_address", 1111, 22, "user_name", "user_password", "root_topic");
     int argc = 2;
     char *argv[] = {
         (char *)"test",
@@ -82,45 +100,28 @@ TEST(GetOptions, GetOptionsHelp)
 
     EXPECT_EQ(ret_val, 1);
 
-    // Check if the mqtt_cfg is not changed
-    EXPECT_EQ(mqtt_cfg._clientName, mqtt_cfg_vanilla._clientName);
-    EXPECT_EQ(mqtt_cfg._host, mqtt_cfg_vanilla._host);
-    EXPECT_EQ(mqtt_cfg._port, mqtt_cfg_vanilla._port);
-    EXPECT_EQ(mqtt_cfg._keepAlive, mqtt_cfg_vanilla._keepAlive);
-    EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
-    EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
+    EXPECT_EQ(checkIfMqttCfgIsNotChanged(mqtt_cfg, mqtt_cfg_vanilla), 0);
 
     // Check if the help message is printed
     EXPECT_NE(output.find("Usage: test [-b broker] [-p port] [-k keepAlive] [-u user] [-w password] [-r topicRoot] [-h help]"), std::string::npos);
 }
 
-TEST(GetOptions, GetOptionsNoArguments)
+TEST_F(GetOptionsTest, GetOptionsNoArguments)
 {
-    MqttCfg mqtt_cfg("client_name", "host_address", 1111, 22, "user_name", "user_password", "root_topic");
     int argc = 1;
     char *argv[] = {
         (char *)"test"};
 
     EXPECT_EQ(getOptions(mqtt_cfg, argc, argv), 0);
 
-    // Check if the mqtt_cfg is not changed
-    EXPECT_EQ(mqtt_cfg._clientName, mqtt_cfg_vanilla._clientName);
-    EXPECT_EQ(mqtt_cfg._host, mqtt_cfg_vanilla._host);
-    EXPECT_EQ(mqtt_cfg._port, mqtt_cfg_vanilla._port);
-    EXPECT_EQ(mqtt_cfg._keepAlive, mqtt_cfg_vanilla._keepAlive);
-    EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
-    EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
+    EXPECT_EQ(checkIfMqttCfgIsNotChanged(mqtt_cfg, mqtt_cfg_vanilla), 0);
+
 
     int argc2 = 0;
     char *argv2[] = {};
 
     EXPECT_EQ(getOptions(mqtt_cfg, argc2, argv2), 0);
 
-    // Check if the mqtt_cfg is not changed
-    EXPECT_EQ(mqtt_cfg._clientName, mqtt_cfg_vanilla._clientName);
-    EXPECT_EQ(mqtt_cfg._host, mqtt_cfg_vanilla._host);
-    EXPECT_EQ(mqtt_cfg._port, mqtt_cfg_vanilla._port);
-    EXPECT_EQ(mqtt_cfg._keepAlive, mqtt_cfg_vanilla._keepAlive);
-    EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
-    EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
+    EXPECT_EQ(checkIfMqttCfgIsNotChanged(mqtt_cfg, mqtt_cfg_vanilla), 0);
+
 }
