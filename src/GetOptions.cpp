197fb17bb908f3aeb027e33432e1b1a570b138f7
Pawel Kawula <pawel.kawula@merit-automotive.com>
2024-10-29 10:41:51 +0100

    Refactor GetOptions.cpp - Improve command line options handling

--- src/GetOptions.cpp
+++ src/GetOptions.cpp
@@ -15,7 +15,6 @@
 int getOptions(MqttCfg &mqtt_cfg, int argc, char *argv[])
 {
     MqttCfg mqtt_cfg_tmp = mqtt_cfg;
-    int ret_val = 0;
     int opt;
 
     while ((opt = getopt(argc, argv, "b:p:k:u:w:r:h")) != -1)
@@ -41,14 +40,18 @@ int getOptions(MqttCfg &mqtt_cfg, int argc, char *argv[])
             mqtt_cfg_tmp._mqttRootTopic = optarg;
             break;
         case 'h':
+            // fprintf(stdout, "Usage: %s [-b broker] [-p port] [-k keepAlive] [-u user] [-w password] [-r topicRoot] [-h help]\n", argv[0]);
+            std::cout << "Usage: " << argv[0] << " [-b broker] [-p port] [-k keepAlive] [-u user] [-w password] [-r topicRoot] [-h help]\n";
+            return 1;
         case '?':
         default:
-            Logger::LogError("Invalid option!");
-            fprintf(stderr, "Usage: %s [-b broker] [-p port] [-k keepAlive] [-u user] [-w password] [-r topicRoot] [-h help]\n", argv[0]);
+            // Logger::LogError("Invalid option!");
+            std::cout << "Usage: " << argv[0] << " [-b broker] [-p port] [-k keepAlive] [-u user] [-w password] [-r topicRoot] [-h help]\n";
+            // fprintf(stderr, "Usage: %s [-b broker] [-p port] [-k keepAlive] [-u user] [-w password] [-r topicRoot] [-h help]\n", argv[0]);
             return -1;
         }
     }
 
     mqtt_cfg = mqtt_cfg_tmp;
-    return ret_val;
+    return 0;
 }
--- test/GetOptions_test.cpp
+++ test/GetOptions_test.cpp
@@ -63,3 +63,64 @@ TEST(GetOptions, GetOptionsInvalid)
     EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
     EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
 }
+
+TEST(GetOptions, GetOptionsHelp)
+{
+    MqttCfg mqtt_cfg("client_name", "host_address", 1111, 22, "user_name", "user_password", "root_topic");
+    int argc = 2;
+    char *argv[] = {
+        (char *)"test",
+        (char *)"-h"};
+
+    // Capture stdout
+    testing::internal::CaptureStdout();
+
+    int ret_val = getOptions(mqtt_cfg, argc, argv);
+
+    // Stop capturing stdout
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(ret_val, 1);
+
+    // Check if the mqtt_cfg is not changed
+    EXPECT_EQ(mqtt_cfg._clientName, mqtt_cfg_vanilla._clientName);
+    EXPECT_EQ(mqtt_cfg._host, mqtt_cfg_vanilla._host);
+    EXPECT_EQ(mqtt_cfg._port, mqtt_cfg_vanilla._port);
+    EXPECT_EQ(mqtt_cfg._keepAlive, mqtt_cfg_vanilla._keepAlive);
+    EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
+    EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
+
+    // Check if the help message is printed
+    EXPECT_NE(output.find("Usage: test [-b broker] [-p port] [-k keepAlive] [-u user] [-w password] [-r topicRoot] [-h help]"), std::string::npos);
+}
+
+TEST(GetOptions, GetOptionsNoArguments)
+{
+    MqttCfg mqtt_cfg("client_name", "host_address", 1111, 22, "user_name", "user_password", "root_topic");
+    int argc = 1;
+    char *argv[] = {
+        (char *)"test"};
+
+    EXPECT_EQ(getOptions(mqtt_cfg, argc, argv), 0);
+
+    // Check if the mqtt_cfg is not changed
+    EXPECT_EQ(mqtt_cfg._clientName, mqtt_cfg_vanilla._clientName);
+    EXPECT_EQ(mqtt_cfg._host, mqtt_cfg_vanilla._host);
+    EXPECT_EQ(mqtt_cfg._port, mqtt_cfg_vanilla._port);
+    EXPECT_EQ(mqtt_cfg._keepAlive, mqtt_cfg_vanilla._keepAlive);
+    EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
+    EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
+
+    int argc2 = 0;
+    char *argv2[] = {};
+
+    EXPECT_EQ(getOptions(mqtt_cfg, argc2, argv2), 0);
+
+    // Check if the mqtt_cfg is not changed
+    EXPECT_EQ(mqtt_cfg._clientName, mqtt_cfg_vanilla._clientName);
+    EXPECT_EQ(mqtt_cfg._host, mqtt_cfg_vanilla._host);
+    EXPECT_EQ(mqtt_cfg._port, mqtt_cfg_vanilla._port);
+    EXPECT_EQ(mqtt_cfg._keepAlive, mqtt_cfg_vanilla._keepAlive);
+    EXPECT_EQ(mqtt_cfg._username, mqtt_cfg_vanilla._username);
+    EXPECT_EQ(mqtt_cfg._password, mqtt_cfg_vanilla._password);
+}
